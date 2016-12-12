b0f9d799cbf008701c69eeaa0ab4c987f64d8b9a
Konstantin Yegupov <kyegupov4@gmail.com>
2016-12-12 00:18:02 +0000

    Replace ktor with sparkjava

--- src/main/kotlin/org/kyegupov/dictionary/server/server.kt
+++ src/main/kotlin/org/kyegupov/dictionary/server/server.kt
@@ -1,22 +1,12 @@
 package org.kyegupov.dictionary.server
 
-import org.jetbrains.ktor.application.*
-import org.jetbrains.ktor.content.TextContent
-import org.jetbrains.ktor.content.resolveClasspathWithPath
-import org.jetbrains.ktor.content.serveClasspathResources
-import org.jetbrains.ktor.features.http.DefaultHeaders
-import org.jetbrains.ktor.features.install
-import org.jetbrains.ktor.http.ContentType
-import org.jetbrains.ktor.jetty.embeddedJettyServer
-import org.jetbrains.ktor.logging.CallLogging
-import org.jetbrains.ktor.logging.SLF4JApplicationLog
-import org.jetbrains.ktor.routing.get
-import org.jetbrains.ktor.routing.route
-import org.jetbrains.ktor.routing.routing
 import org.kyegupov.dictionary.tools.GSON
 import org.kyegupov.dictionary.tools.Language
 import java.io.InputStreamReader
 import java.util.*
+import spark.Request
+import spark.Response
+import spark.Spark
 
 data class DictionaryOfStringArticles(
         val entries: List<String>,
@@ -35,10 +25,6 @@ data class PhraseWord(
         val normalizedWord: String? // empty if non-translatable
 )
 
-fun ApplicationCall.respondJson(value: Any): Nothing {
-    respond(TextContent(ContentType.Application.Json, GSON.toJson(value)))
-}
-
 val NON_WORD_CHARS = Regex("\\b")
 
 val ENDING_NORMALIZATION = listOf<Pair<List<String>, String>>(
@@ -46,102 +32,82 @@ val ENDING_NORMALIZATION = listOf<Pair<List<String>, String>>(
         Pair(listOf("as", "is", "os", "us"), "ar")
 )
 
-class JsonApplication(environment: ApplicationEnvironment) : Application(environment) {
-
-    init {
-        install(DefaultHeaders)
-        install(CallLogging)
-
-        val allLanguageCodes = mapOf(Pair("i", Language.IDO), Pair("e", Language.ENGLISH))
-
-        val data = mutableMapOf<Language, DictionaryOfStringArticles>()
-
-        for ((langCode, lang) in allLanguageCodes)
-        {
-            this.javaClass.classLoader.getResourceAsStream("dyer_bundle/$langCode/combined.json").use {
-                val reader = InputStreamReader(it);
-                val dataAsJson = GSON.fromJson(reader, Map::class.java)
-                val gsonMap = dataAsJson["index"] as Map<String, List<Int>>
-                data[lang] = DictionaryOfStringArticles(
-                        entries = dataAsJson["articles"] as List<String>,
-                        compactIndex = TreeMap(gsonMap))
-            }
-        }
-
-        routing {
-
-            get("api/search") {
-                // TODO: support multiple non-Ido languages, get language from client
-                val query = call.request.queryParameters["query"]!!
-                val result = mutableMapOf<String, PerLanguageSearchResponse>()
-                val queryLang = call.request.queryParameters["lang"]
-                val languages = if (queryLang == null) allLanguageCodes.keys else listOf(queryLang)
-                for (langCode in languages) {
-                    val lang = allLanguageCodes[langCode]
-                    val dic = data[lang]!!
-                    val suggestedWords: Map<String, List<Int>> = dic.compactIndex.subMap(query, query + "\uFFFF")
-                    val preciseArticleIds = dic.compactIndex[query] ?:
-                            if (suggestedWords.size == 1) dic.compactIndex[suggestedWords.entries.first().key]!!
-                            else listOf()
-                    val langResult = PerLanguageSearchResponse(
-                            suggestions = if (suggestedWords.entries.size < 100) {
-                                suggestedWords.entries.take(30).map { it.key }
-                            } else listOf<String>(),
-                            totalSuggestions = suggestedWords.size,
-                            articlesHtml = preciseArticleIds.map { dic.entries[it] })
-                    result[langCode] = langResult
-                }
-                call.respondJson(result)
-            }
-
-            get("api/phrase") {
-                // TODO: support multiple non-Ido languages, get language from client
-                val query = call.request.queryParameters["query"]!!
-                val phraseResult = mutableListOf<PhraseWord>()
-                val dic = data[Language.IDO]!!
-                for (word0 in query.split(NON_WORD_CHARS)) {
-                    val word = normalizeIdoWord(word0.toLowerCase())
-                    if (dic.compactIndex.containsKey(word)) {
-                        phraseResult.add(PhraseWord(word0, word))
-                    } else {
-                        phraseResult.add(PhraseWord(word0, null))
-                    }
-                }
-                call.respondJson(phraseResult)
-            }
-
-            route("/static/") {
-                serveClasspathResources("static_site/client_server")
-            }
-            route("/") {
-                handle {
-                    call.resolveClasspathWithPath("static_site/client_server", "index.html")?.let {
-                        call.respond(it)
-                    }
-                }
+// TODO: handle adjectives without -a
+private fun normalizeIdoWord(word: String): String? {
+    for (pair in ENDING_NORMALIZATION) {
+        for (endingInflected in pair.first) {
+            if (word.endsWith(endingInflected)) {
+                return word.removeSuffix(endingInflected) + pair.second
             }
         }
     }
-
-    // TODO: handle adjectives without -a
-    private fun normalizeIdoWord(word: String): String? {
-        for (pair in ENDING_NORMALIZATION) {
-            for (endingInflected in pair.first) {
-                if (word.endsWith(endingInflected)) {
-                    return word.removeSuffix(endingInflected) + pair.second
-                }
-            }
-        }
-        return word
-    }
+    return word
 }
 
+class Dummy {}
+val CLASS_LOADER = Dummy::class.java.classLoader
+
 fun main(args: Array<String>) {
-    // TODO: ktor turned out to be a bad choice of framework - overcomplicated internals, buggy, poorly documented,
-    // developer-unfriendly.
-    // If the situation does not improve drastically after next (post-0.2.2) release, consider switching to:
-    // Wasabi, Kara, SparkFramework, Dropwizard
-    embeddedJettyServer(3000,
-            application = JsonApplication(applicationEnvironment { log = SLF4JApplicationLog("ktor") }))
-            .start()
+    val allLanguageCodes = mapOf(Pair("i", Language.IDO), Pair("e", Language.ENGLISH))
+
+    val data = mutableMapOf<Language, DictionaryOfStringArticles>()
+
+    for ((langCode, lang) in allLanguageCodes)
+    {
+        CLASS_LOADER.getResourceAsStream("dyer_bundle/$langCode/combined.json").use {
+            val reader = InputStreamReader(it);
+            val dataAsJson = GSON.fromJson(reader, Map::class.java)
+            val gsonMap = dataAsJson["index"] as Map<String, List<Int>>
+            data[lang] = DictionaryOfStringArticles(
+                    entries = dataAsJson["articles"] as List<String>,
+                    compactIndex = TreeMap(gsonMap))
+        }
+    }
+
+    Spark.port(3000)
+
+    // TODO: serve from fs when not in a jar
+    Spark.staticFiles.location("static_site/client_server")
+
+    Spark.get("api/search", { request: Request, response: Response ->
+        // TODO: support multiple non-Ido languages, get language from client
+        val query = request.queryParams("query")!!
+        val result = mutableMapOf<String, PerLanguageSearchResponse>()
+        val queryLang = request.queryParams("lang")
+        val languages = if (queryLang == null) allLanguageCodes.keys else listOf(queryLang)
+        for (langCode in languages) {
+            val lang = allLanguageCodes[langCode]
+            val dic = data[lang]!!
+            val suggestedWords: Map<String, List<Int>> = dic.compactIndex.subMap(query, query + "\uFFFF")
+            val preciseArticleIds = dic.compactIndex[query] ?:
+                    if (suggestedWords.size == 1) dic.compactIndex[suggestedWords.entries.first().key]!!
+                    else listOf()
+            val langResult = PerLanguageSearchResponse(
+                    suggestions = if (suggestedWords.entries.size < 100) {
+                        suggestedWords.entries.take(30).map { it.key }
+                    } else listOf<String>(),
+                    totalSuggestions = suggestedWords.size,
+                    articlesHtml = preciseArticleIds.map { dic.entries[it] })
+            result[langCode] = langResult
+        }
+        response.type("application/json")
+        GSON.toJson(result)
+    })
+
+    Spark.get("api/phrase", { request: Request, response: Response ->
+        // TODO: support multiple non-Ido languages, get language from client
+        val query = request.queryParams("query")!!
+        val phraseResult = mutableListOf<PhraseWord>()
+        val dic = data[Language.IDO]!!
+        for (word0 in query.split(NON_WORD_CHARS)) {
+            val word = normalizeIdoWord(word0.toLowerCase())
+            if (dic.compactIndex.containsKey(word)) {
+                phraseResult.add(PhraseWord(word0, word))
+            } else {
+                phraseResult.add(PhraseWord(word0, null))
+            }
+        }
+        response.type("application/json")
+        GSON.toJson(phraseResult)
+    })
 }
