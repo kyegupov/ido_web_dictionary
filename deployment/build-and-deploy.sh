1a183685cd99f725e908ab1bbfc6f1e7888b7409
Konstantin Yegupov <kyegupov4@gmail.com>
2017-11-04 18:43:27 +0000

    Updated build script

--- deployment/build-and-deploy.sh
+++ deployment/build-and-deploy.sh
@@ -1,6 +1,6 @@
 # Builds and deploys the current version of the service.
 #
-# Before running this, install ido_dictionary.service via systemd
+# Before running this, install ido_dictionary.service via " "
 # and configure your nginx as per excerpt
 
 # Run this from root of the repository
@@ -9,6 +9,8 @@ git pull
 cargo build --release
 mkdir -p /opt/ido_web_dictionary/
 cp target/release/ido_web_dictionary /opt/ido_web_dictionary/
+mkdir -p /opt/ido_web_dictionary/backend/src/main/resources/dictionaries_by_letter/
+cp -r backend/src/main/resources/dictionaries_by_letter/* /opt/ido_web_dictionary/backend/src/main/resources/dictionaries_by_letter/*
 chmod go+rx -R /opt/ido_web_dictionary/
 systemctl restart ido_dictionary
 
